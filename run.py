e61989b1e9b3a629bf43dee0b2bbc3ffeef0e6f5
ahmed.raza <ahmed.raza@unikrew.com>
2024-08-19 16:10:14 +0500

    Prettier Formatting

--- run.py
+++ run.py
@@ -1,23 +1,20 @@
 from flaskBlog import app
 
 if __name__ == '__main__':
-    app.run(debug=True)    
-
-
+    app.run(debug=True)
 
 """ Description """
 
-#Create a Virtual Environment: python -m venv .venv
+# Create a Virtual Environment: python -m venv .venv
 
-#2. If Virtual Environment Already Created: First Activate the Virtual Environment: .\.venv\Scripts\activate
+# 2. If Virtual Environment Already Created: First Activate the Virtual Environment: .\.venv\Scripts\activate
 
 # To run application directly using python: python run.py
 
 # debug=True means run app in Debug Mode
 
-#__name__ == '__main__':
+# __name__ == '__main__':
 """ 
 Ye if wali condition check karti hai ke agar ye script directly run ho rahi hai (matlab, import nahi ho rahi dusri file se), 
 toh app.run(debug=True) run ho otherwise python flaskBlog.py command not work
 """
-
